874db6984ef9edb0b3d88348b857044ea6ae95d7
Robson Correia <robson.correia.bd@gmail.com>
2022-11-04 11:13:32 -0300

    ✨feat: game over and you win func

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import pyxel
 import random
 from math import sqrt
 
-COLOR = pyxel.COLOR_DARK_BLUE
+COLOR_ACTIVE = pyxel.COLOR_GREEN
+COLOR_SLEEP = pyxel.COLOR_RED
 STEP = 2
 RADIUS = 5
+WIDTH = 160
+HEIGTH = 120
+TITLE = "Robson Correia"
+GAME_OVER = "GAME OVER!"
+YOU_WIN = "YOU WIN!"
 
+pyxel.game_over = False
 
 class App:
     def __init__(self):
 
-        pyxel.init(160, 120, title="Robson Correia")
+        pyxel.init(WIDTH, HEIGTH, TITLE)
 
         pyxel.sleep1 = False
         pyxel.sleep2 = False
@@ -19,20 +26,34 @@ class App:
         pyxel.x1 = random.uniform(20, 140)
         pyxel.x2 = random.uniform(20, 140)
         pyxel.x3 = random.uniform(20, 140)
+
         pyxel.y1 = random.uniform(20, 100)
         pyxel.y2 = random.uniform(20, 100)
         pyxel.y3 = random.uniform(20, 100)
 
         pyxel.mouse(True)
+        
         pyxel.run(self.update, self.draw)
 
     def in_circle(arg, x, y, cx, cy):
         dist = sqrt((x - cx) ** 2 + (y - cy) ** 2)
         return dist <= RADIUS
 
+    def you_win(arg, x1, x2, x3):
+        return x1 == x2 == x3 == True
+
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+
+        if pyxel.frame_count > 30 * 5:
+            pyxel.game_over = True
+
+        if pyxel.game_over:
+            return
+
+        if self.you_win(pyxel.sleep1, pyxel.sleep2, pyxel.sleep3):
+            return
 
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             if self.in_circle(pyxel.mouse_x, pyxel.mouse_y, pyxel.x1, pyxel.y1):
@@ -55,10 +76,26 @@ class App:
             pyxel.y3 = pyxel.y3 + random.uniform(-STEP, STEP)
 
     def draw(self):
-        pyxel.cls(pyxel.COLOR_BLACK)
-        pyxel.circ(pyxel.x1, pyxel.y1, RADIUS, COLOR)
-        pyxel.circ(pyxel.x2, pyxel.y2, RADIUS, COLOR)
-        pyxel.circ(pyxel.x3, pyxel.y3, RADIUS, COLOR)
 
+        pyxel.cls(pyxel.COLOR_BLACK)
+
+        pyxel.circ(pyxel.x1, pyxel.y1, RADIUS,
+                   COLOR_SLEEP if pyxel.sleep1 else COLOR_ACTIVE)
+        pyxel.circ(pyxel.x2, pyxel.y2, RADIUS,
+                   COLOR_SLEEP if pyxel.sleep2 else COLOR_ACTIVE)
+        pyxel.circ(pyxel.x3, pyxel.y3, RADIUS,
+                   COLOR_SLEEP if pyxel.sleep3 else COLOR_ACTIVE)
+
+        if self.you_win(pyxel.sleep1, pyxel.sleep2, pyxel.sleep3):
+            x = (WIDTH // 2) - (len(YOU_WIN) // 2) * pyxel.FONT_WIDTH
+            y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
+            pyxel.text(x, y, YOU_WIN, pyxel.frame_count % 16)
+            return
+
+        if pyxel.game_over:
+            x = (WIDTH // 2) - (len(GAME_OVER) // 2) * pyxel.FONT_WIDTH
+            y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
+            pyxel.text(x, y, GAME_OVER, pyxel.frame_count % 16)
+            return
 
 App()
