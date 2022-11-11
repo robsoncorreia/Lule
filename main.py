ee3e7fd39828884a91e6c1f9e832b1cb75d746b6
Robson Correia <robson.correia.bd@gmail.com>
2022-11-10 22:21:05 -0300

    ✨ feat: add scene

--- main.py
+++ main.py
@@ -13,9 +13,27 @@ YOU_WIN = "YOU WIN!"
 CIRCLES = 20
 BORDER = 20
 FPS = 30
+TEXTO_DE_ABERTURA = "Antes de tudo, gostaria de te avisar\nque essa nao vai ser uma historia\ncomo as outras."
 
 
 pyxel.game_over = False
+
+
+class Text:
+    def __init__(self, x, y, text) -> None:
+        self.x = x
+        self.y = y
+        self.text = text
+        self.lenght = len(self.text)
+
+
+class Image:
+    def __init__(self, x, y, w, h, id):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.id = id
 
 
 class Circle:
@@ -42,6 +60,10 @@ class App:
         self.score = 0
 
         self.gameOver = False
+
+        self.lulaImage = Image(0, 32, 64, 64, 0)
+
+        self.text = Text(8, 8, TEXTO_DE_ABERTURA[0])
 
         pyxel.init(WIDTH, HEIGTH, TITLE, FPS)
 
@@ -118,28 +140,39 @@ class App:
                       0 if not circle.isClicked else 16,
                       0 if not circle.isClicked else 32, 8, 8)
 
+    def scene(self):
+        if (self.lulaImage.x < ((WIDTH // 2) - self.lulaImage.w // 2)) and pyxel.frame_count % 3 == 0:
+            self.lulaImage.x = self.lulaImage.x + 1
+        pyxel.blt(self.lulaImage.x, self.lulaImage.y, 2,
+                  0, 0, self.lulaImage.w, self.lulaImage.h)
+        if len(self.text.text) < len(TEXTO_DE_ABERTURA) and pyxel.frame_count % 2 == 0:
+            self.text.text = TEXTO_DE_ABERTURA[0: len(self.text.text) + 1]
+        pyxel.text(self.text.x, self.text.y,
+                   self.text.text, pyxel.COLOR_WHITE)
+
     def draw(self):
 
         pyxel.cls(pyxel.COLOR_BLACK)
+        self.scene()
 
-        if self.you_win():
-            x = (WIDTH // 2) - (len(YOU_WIN) // 2) * pyxel.FONT_WIDTH
-            y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
-            pyxel.text(x, y, YOU_WIN, pyxel.frame_count % 16)
-            return
+        # if self.you_win():
+        #     x = (WIDTH // 2) - (len(YOU_WIN) // 2) * pyxel.FONT_WIDTH
+        #     y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
+        #     pyxel.text(x, y, YOU_WIN, pyxel.frame_count % 16)
+        #     return
 
-        if self.gameOver:
-            x = (WIDTH // 2) - (len(GAME_OVER) // 2) * pyxel.FONT_WIDTH
-            y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
-            pyxel.text(x, y, GAME_OVER, pyxel.frame_count % 16)
-            return
-        else:
-            pyxel.text(
-                2, 2, f'Time: {self.countdown - pyxel.frame_count}', pyxel.frame_count % 16)
-            pyxel.text(
-                2, 8, f'Score: {self.score}', pyxel.frame_count % 16)
+        # if self.gameOver:
+        #     x = (WIDTH // 2) - (len(GAME_OVER) // 2) * pyxel.FONT_WIDTH
+        #     y = (HEIGTH // 2) - pyxel.FONT_HEIGHT
+        #     pyxel.text(x, y, GAME_OVER, pyxel.frame_count % 16)
+        #     return
+        # else:
+        #     pyxel.text(
+        #         2, 2, f'Time: {self.countdown - pyxel.frame_count}', pyxel.frame_count % 16)
+        #     pyxel.text(
+        #         2, 8, f'Score: {self.score}', pyxel.frame_count % 16)
 
-        self.draw_circle()
+        # self.draw_circle()
 
 
 App()
